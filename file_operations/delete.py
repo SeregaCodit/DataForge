789b2feb42e56cd4597b8b57a01f56922a2c5cda
Serhii Naumenko <naumenko.s.mail@gmail.com>
2026-01-21 17:04:44 +0200

    feat(commands): add DeleteOperation class

---
+++ file_operations/delete.py
@@ -0,0 +1,22 @@
+import argparse
+import shutil
+
+from const_utils.arguments import Arguments
+from const_utils.parser_help import HelpStrings
+from file_operations.file_operation import FileOperation
+
+
+class DeleteOperation(FileOperation):
+    """Delete files that match the pattern"""
+    @staticmethod
+    def add_arguments(parser: argparse.ArgumentParser) -> None:
+        """this class doesn't have unique arguments"""
+        pass
+
+    def do_task(self):
+        """Delete files that match the pattern"""
+        for file_path in self.files_for_task:
+            if file_path.is_file():
+                print(f"[{self.__class__.__name__}] deleting {file_path}", end=" ")
+                file_path.unlink(missing_ok=True)
+                print("[OK]")
