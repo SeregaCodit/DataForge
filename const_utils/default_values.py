5fe443d5bb665ef4a449918d3a46449d8292364d
Serhii Naumenko <naumenko.s.mail@gmail.com>
2026-01-21 11:20:10 +0200

    doc(defaults): add docstring for DefaultValuse class

--- const_utils/default_values.py
+++ const_utils/default_values.py
@@ -4,6 +4,7 @@ from typing import Union
 
 @dataclass
 class DefaultValues:
+    """Default values of positional arguments"""
     pattern: tuple = ()
     sleep: Union[int, bool] = 60
     type: str = ".jpg"
