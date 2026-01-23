d457e0d9a10b74b9ce6e873b201fb57ed9080826
Serhii Naumenko <naumenko.s.mail@gmail.com>
2026-01-23 18:33:13 +0200

    fix(compare):  fix circular imports

--- tools/comparer/img_comparer/img_comparer.py
+++ tools/comparer/img_comparer/img_comparer.py
@@ -5,10 +5,10 @@ from imagededup.methods import PHash, AHash, DHash, CNN
 
 from const_utils.copmarer import Constants
 from logger.logger import LoggerConfigurator
-from tools.comparer.comparer import Comparer
 
 
-class ImageComparer(Comparer):
+
+class ImageComparer:
     """Abstract class for comparing images"""
     def __init__(self, method_name: str = Constants.phash, log_path: Union[Path, None] = None):
         super().__init__()
@@ -18,17 +18,18 @@ class ImageComparer(Comparer):
             Constants.ahash: AHash,
             Constants.cnn: CNN
         }
-        self.method = self.method_mapping[method_name]
+        self.method = self.method_mapping[method_name]()
         self.logger = LoggerConfigurator.setup(
             name=self.__class__.__name__,
             log_path=Path(log_path) / f"{self.__class__.__name__}.log" if log_path else None
         )
         self.outfile = Path(log_path) / f"{self.__class__.__name__}.json" if log_path else None
 
-    def compare_files(self, image_dir: Path, threshold: int = 10, outfile: Optional[Path, str] = None) -> list:
+    def compare_files(self, image_dir: Path, threshold: int = 10) -> list:
         duplicates = self.method.find_duplicates_to_remove(
             image_dir=image_dir,
             max_distance_threshold=threshold,
-            outfile=outfile
+            outfile=self.outfile
         )
+        self.logger.info(f"Duplicates: [{len(duplicates)}] {duplicates}")
         return duplicates
