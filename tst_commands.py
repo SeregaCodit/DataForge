5a7ea37aa3ea7e68697a4a57989bedab0476ade3
Serhii Naumenko <naumenko.s.mail@gmail.com>
2026-01-24 13:43:05 +0200

    refactor(tests):  add command mapping instead commenting rows

--- tst_commands.py
+++ tst_commands.py
@@ -1,67 +1,49 @@
 import sys
 from pathlib import Path
 
+from const_utils.commands import Commands
 from fileManager import FileManager
 
 script_path = Path(__file__).parent / "fileManager.py"
 
-
-if __name__ == "__main__":
-    # sys.path.append(str(Path(__file__).parent / "fileManager"))
-    # Емулюємо введення в терміналі:
-    # -----SLICE-----
-    # sys.argv = [
-    #     str(script_path),
-    #     "slice",
-    #     "./media/",
-    #     "./media/imgs/",
-    #     "-p", ".mp4", ".MP4",
-    #     "-t", ".jpg",
-    #     # "-r",
-    #     "-s", "60",
-    #     "-step", "1",
-    # ]
-
-    # -----SLICE WITH DELETING-----
-    # sys.argv = [
-    #     "fileManager.py",
-    #     "slice",
-    #     "./media/imgs/",
-    #     "./media/imgs/",
-    #     "-p", ".mp4", ".MP4",
-    #     "-t", ".jpg",
-    #     # "-r",
-    #     "-rm",
-    #     "-s", "60",
-    #     "-step", "1",
-    # ]
-
-    # #-----MOVE-----
-    # sys.argv = [
-    #     "fileManager.py",
-    #     "move",
-    #     "./media/imgs/",
-    #     "./media/imgs_new/",
-    #     "-p", ".jpg",
-    #     "-r",
-    #     "-s", "30"
-    # ]
-    # -----DELETE-----
-    # sys.argv = [
-    #     "fileManager.py",
-    #     "delete",
-    #     "./media/imgs_new/",
-    #     "-p", ".jpg",
-    #-----DEDUP-----
-    sys.argv = [
+MAPPING = {
+    Commands.slice: [
+        str(script_path),
+        "slice",
+        "./media/",
+        "./media/imgs/",
+        "-p", ".mp4", ".MP4",
+        "-t", ".jpg",
+        # "-r",
+        "-s", "60",
+        "-step", "1",
+    ],
+    Commands.delete: [
+        "fileManager.py",
+        "delete",
+        "./media/imgs_new/",
+        "-p", ".jpg",
+    ],
+    Commands.move: [
+        "fileManager.py",
+        "move",
+        "./media/imgs/",
+        "./media/imgs_new/",
+        "-p", ".jpg",
+        "-r",
+        "-s", "30"
+    ],
+    Commands.dedup: [
         "fileManager.py",
         "dedup",
         "./media/imgs/",
-        # "./media/imgs_new/",
         "-p", ".jpg", ".png",
         "--filetype", "image",
         "--threshold", "10",
-        "-rm"
     ]
+}
+
+if __name__ == "__main__":
+    sys.argv = MAPPING[Commands.dedup]
     app = FileManager()
     app.execute()
