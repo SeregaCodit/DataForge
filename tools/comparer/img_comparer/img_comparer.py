5dc21a029b4647dbe1efa773775cc456ef0cd7aa
Serhii Naumenko <naumenko.s.mail@gmail.com>
2026-01-24 12:15:42 +0200

    docs(docs):  add documentation

--- tools/comparer/img_comparer/img_comparer.py
+++ tools/comparer/img_comparer/img_comparer.py
@@ -11,14 +11,24 @@ class ImageComparer:
     def __init__(
             self,
             method_name: str = Constants.phash,
-            log_path: Union[Path, None] = None
+            log_path: Union[Path, None] = None,
+            threshold_percentage: float = 10,
     ):
+        """
+        An orchestrator for comparing two images using principial different algorithms.
+        :param method_name: duplicate search method name
+        :param log_path: path to log file
+        :param threshold_percentage: percentage of duplicate hash value to consider as duplicate
+        """
         super().__init__()
         self.method_mapping = {
             Constants.dhash: DHash,
         }
 
-        self.method = self.method_mapping[method_name]()
+        self.method = self.method_mapping[method_name](
+            hash_size=16,
+            threshold=threshold_percentage
+        )
 
         self.logger = LoggerConfigurator.setup(
             name=self.__class__.__name__,
@@ -26,6 +36,11 @@ class ImageComparer:
         )
 
     def compare(self, file_paths: Tuple[Path]):
+        """
+        compare files via each-with-each method
+        :param file_paths: paths of files to be compared
+        :return: matches that satisfy threshold condition
+        """
         hash_map = self.method.get_hashmap(file_paths)
-        duplicates = self.method.find_duplicates(hash_map)
-        return duplicates
+        matches = self.method.find_duplicates(hash_map)
+        return matches
