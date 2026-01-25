c0f98d4467111b96eebc5471aa4af057c4385290
Serhii Naumenko <naumenko.s.mail@gmail.com>
2026-01-25 17:53:01 +0200

    feat(cache): create cache io class for caching files and reading them

---
+++ tools/cache.py
@@ -0,0 +1,60 @@
+import hashlib
+import pickle
+from pathlib import Path
+from typing import Union, Dict, BinaryIO
+import numpy as np
+
+from const_utils.default_values import DefaultValues
+from file_operations.file_remover import FileRemoverMixin
+from logger.logger import LoggerConfigurator
+from logger.logger_protocol import LoggerProtocol
+
+
+class CacheIO:
+    def __init__(self):
+        """
+        saving and reading cache files for faster loading data
+        """
+        self.logger = LoggerConfigurator.setup(
+            name=self.__class__.__name__,
+            log_path=Path(DefaultValues.log_path) / f"{self.__class__.__name__}.log",
+            log_level=DefaultValues.log_level
+        )
+
+
+    def load(self: LoggerProtocol, cache_file) -> Dict[Path, np.ndarray]:
+        """loading cache file"""
+        try:
+            self.logger.info(f"Loading cache file {cache_file}")
+            with open(cache_file, "rb") as file:
+                return pickle.load(file)
+
+        except FileNotFoundError:
+            self.logger.warning(f"Cache file {cache_file} does not exist")
+            return {}
+        except EOFError:
+            self.logger.warning(f"Cache file {cache_file} is damaged. Deleting cache file")
+            return {}
+
+    def save(self: LoggerProtocol, hash_map: Dict[Path, np.ndarray], cache_file: Path) -> None:
+        """save hashmap to cache file"""
+        cache_file.parent.mkdir(parents=True, exist_ok=True)
+        self.logger.info(f"Saving cache file {cache_file}")
+
+        with open(cache_file, 'wb') as file:
+            pickle.dump(hash_map, file)
+
+        self.logger.info(f"Cache file {cache_file} saved")
+
+    @staticmethod
+    def generate_cache_filename(source_path: Path, hash_type: str, core_size: int) -> str:
+        # 1. Беремо абсолютний шлях, щоб уникнути плутанини між "./media" та "/home/user/media"
+        abs_path = str(source_path.resolve())
+
+        # 2. Генеруємо MD5 хеш від шляху (потрібно закодувати рядок у байти)
+        path_hash = hashlib.md5(abs_path.encode('utf-8')).hexdigest()
+
+        # 3. Формуємо фінальне ім'я
+        # Додаємо core_size та hash_type в назву, щоб при зміні налаштувань кеш автоматично "скидався"
+        return f"cache_{path_hash}_{hash_type}_s{core_size}.pkl"
+
